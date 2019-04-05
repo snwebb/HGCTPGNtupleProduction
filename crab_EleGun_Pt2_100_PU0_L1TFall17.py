d10b2ec67198e47d4247d2fe6511279477599b44
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-04-05 15:39:10 +0200

    Update crab configs

--- crab_EleGun_Pt2_100_PU0_L1TFall17.py
+++ crab_EleGun_Pt2_100_PU0_L1TFall17.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('.')
-from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config
 import local
 
-config = Configuration()
+config = config()
 
 config.section_("General")
 config.General.requestName = 'EleGun_Pt2_100_PU0_L1TFall17'
@@ -18,10 +18,10 @@ config.section_("Data")
 config.Data.inputDataset = '/SingleE_FlatPt-2to100/PhaseIIFall17D-L1TnoPU_93X_upgrade2023_realistic_v5-v1/GEN-SIM-DIGI-RAW'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 5
+config.Data.unitsPerJob = 3
 config.Data.outLFNDirBase = local.outLFNDirBase
 config.Data.publication = False
-config.Data.outputDatasetTag = 'EleGun_Pt2_100_PU0_L1TFall17'
+config.Data.outputDatasetTag = config.General.requestName
 
 config.section_("Site")
 config.Site.storageSite = local.storageSite
