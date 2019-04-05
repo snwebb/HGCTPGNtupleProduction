332eb7c121a9468e86bc1bcb48aa9e22b6d7b442
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-04-05 13:57:08 +0200

    Add multi algo configs

--- crab/crab_EleGun_Pt2_100_PU0_L1TFall17.py
+++ crab/crab_EleGun_Pt2_100_PU0_L1TFall17.py
@@ -6,23 +6,22 @@ import local
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'EleGun_Pt5_100_PU0'
+config.General.requestName = 'EleGun_Pt2_100_PU0_L1TFall17'
 config.General.workArea = 'jobs'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'produce_ntuple_reduced_cfg.py'
-config.JobType.allowUndistributedCMSSW = True
+config.JobType.psetName = 'produce_ntuple_multialgos_reduced_genmatch_v8_cfg.py'
+#config.JobType.allowUndistributedCMSSW = True
 
 config.section_("Data")
-config.Data.inputDataset = '/SingleElectronPt5_100Eta1p6_2p8/PhaseIITDRFall17DR-noPUFEVT_93X_upgrade2023_realistic_v2-v1/GEN-SIM-RECO'
+config.Data.inputDataset = '/SingleE_FlatPt-2to100/PhaseIIFall17D-L1TnoPU_93X_upgrade2023_realistic_v5-v1/GEN-SIM-DIGI-RAW'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 5
-#  config.Data.totalUnits = 100
 config.Data.outLFNDirBase = local.outLFNDirBase
 config.Data.publication = False
-config.Data.outputDatasetTag = 'EleGun_Pt5_100_PU0'
+config.Data.outputDatasetTag = 'EleGun_Pt2_100_PU0_L1TFall17'
 
 config.section_("Site")
 config.Site.storageSite = local.storageSite
