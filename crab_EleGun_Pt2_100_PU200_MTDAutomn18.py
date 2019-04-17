4051a1a210cb17311a8dfbd657fcba9e4e318a1b
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-04-17 16:51:31 +0200

    Add electron and pion MTD samples

---
+++ crab_EleGun_Pt2_100_PU200_MTDAutomn18.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('.')
+from CRABClient.UserUtilities import config
+import local
+
+config = config()
+
+config.section_("General")
+config.General.requestName = 'EleGun_Pt2_100_PU200_MTDAutomn18'
+config.General.workArea = 'jobs'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'produce_ntuple_multialgos_reduced_genmatch_v9_cfg.py'
+config.JobType.maxMemoryMB = 2000
+
+config.section_("Data")
+config.Data.inputDataset = '/SingleE_FlatPt-2to100/PhaseIIMTDTDRAutumn18DR-PU200_103X_upgrade2023_realistic_v2-v1/FEVT'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+config.Data.outLFNDirBase = local.outLFNDirBase
+config.Data.publication = False
+config.Data.outputDatasetTag = config.General.requestName
+
+config.section_("Site")
+config.Site.storageSite = local.storageSite
